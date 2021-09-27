14f4808bfab0d934dba3187e528e26207df0e8bd
Jeff Pippins <jeffpippinsgames@gmail.comcd>
2021-09-27 14:33:53 -0700

    Sep 27 14:33

--- tbiweld_vgroovetrackingcontainer.cpp
+++ tbiweld_vgroovetrackingcontainer.cpp
@@ -53,29 +53,54 @@ TBIWeld_ProcessingPipeLineReturnType::PipelineReturnType_t TBIWeld_VGrooveTracki
     return TBIWeld_ProcessingPipeLineReturnType::TBI_PIPELINE_OK;
 }
 
-TBIWeld_ProcessingPipeLineReturnType::PipelineReturnType_t TBIWeld_VGrooveTrackingContainer::extractTrackingPointsForHeightOnly(TBILine &_tsl, TBIClass_OpenCVMatContainer &_mats)
+int TBIWeld_VGrooveTrackingContainer::getXMovement(TBIMotionControlParameters &_motioncontrolparams)
 {
-    m_left_tracking_point = _tsl.getMidPointofLine();
-    cv::drawMarker(_mats.m_operation, m_left_tracking_point.toCVPoint(), m_tracking_point_cv_color,cv::MARKER_CROSS, 15, 1, cv::LINE_AA);
-    return TBIWeld_ProcessingPipeLineReturnType::TBI_PIPELINE_OK;
+    return 0;
 }
 
-void TBIWeld_VGrooveTrackingContainer::setTrackToPoints(GaryControlMode::ControlMode_t _forcontrolmode)
+int TBIWeld_VGrooveTrackingContainer::getZMovement(TBIMotionControlParameters &_motioncontrolparams)
+{
+    return 0;
+}
+
+
+void TBIWeld_VGrooveTrackingContainer::setTrackToPoints()
 {
     m_left_track_to_point = m_left_tracking_point;
-    if(_forcontrolmode == GaryControlMode::TBI_CONTROL_MODE_HEIGHTONLY) return;
     m_right_track_to_point = m_right_tracking_point;
     m_root_track_to_point = m_root_point;
     m_joint_track_to_centroid = m_joint_centroid;
 }
 
-void TBIWeld_VGrooveTrackingContainer::drawTrackToPointstoMat(TBIClass_OpenCVMatContainer &_mats, GaryControlMode::ControlMode_t _forcontrolmode)
+void TBIWeld_VGrooveTrackingContainer::drawTrackToPointstoMat_FullAuto(TBIClass_OpenCVMatContainer &_mats)
 {
-    cv::circle(_mats.m_operation, m_left_track_to_point.toCVPoint(), m_tracking_boundary, m_tracking_to_point_cv_color, 1, cv::LINE_AA);
+    cv::circle(_mats.m_operation, m_left_track_to_point.toCVPoint(), m_movement_boundary, m_tracking_to_point_cv_color, 1, cv::LINE_AA);
     cv::drawMarker(_mats.m_operation, m_left_track_to_point.toCVPoint(), m_tracking_to_point_cv_color,cv::MARKER_DIAMOND, 40, 1, cv::LINE_AA);
-    if(_forcontrolmode == GaryControlMode::TBI_CONTROL_MODE_HEIGHTONLY) return;
-    cv::circle(_mats.m_operation, m_right_track_to_point.toCVPoint(), m_tracking_boundary, m_tracking_to_point_cv_color, 1, cv::LINE_AA);
+    cv::circle(_mats.m_operation, m_right_track_to_point.toCVPoint(), m_movement_boundary, m_tracking_to_point_cv_color, 1, cv::LINE_AA);
     cv::drawMarker(_mats.m_operation, m_right_track_to_point.toCVPoint(), m_tracking_to_point_cv_color,cv::MARKER_DIAMOND, 40, 1, cv::LINE_AA);
-    //cv::drawMarker(_mats.m_operation, m_root_track_to_point.toCVPoint(), m_tracking_to_point_cv_color,cv::MARKER_DIAMOND, 30, 1, cv::LINE_AA);
-    //cv::drawMarker(_mats.m_operation, m_joint_centroid.toCVPoint(), m_tracking_to_point_cv_color,cv::MARKER_DIAMOND, 30, 1, cv::LINE_AA);
 }
+
+void TBIWeld_VGrooveTrackingContainer::drawTrackToPointstoMat_HeightOnly(TBIClass_OpenCVMatContainer &_mats)
+{
+    cv::circle(_mats.m_operation, m_left_track_to_point.toCVPoint(), m_movement_boundary, m_tracking_to_point_cv_color, 1, cv::LINE_AA);
+    cv::drawMarker(_mats.m_operation, m_left_track_to_point.toCVPoint(), m_tracking_to_point_cv_color,cv::MARKER_DIAMOND, 40, 1, cv::LINE_AA);
+    cv::circle(_mats.m_operation, m_right_track_to_point.toCVPoint(), m_movement_boundary, m_tracking_to_point_cv_color, 1, cv::LINE_AA);
+    cv::drawMarker(_mats.m_operation, m_right_track_to_point.toCVPoint(), m_tracking_to_point_cv_color,cv::MARKER_DIAMOND, 40, 1, cv::LINE_AA);
+}
+
+void TBIWeld_VGrooveTrackingContainer::drawTrackingPointsToMat_FullAuto(TBIClass_OpenCVMatContainer &_mats)
+{
+
+}
+
+void TBIWeld_VGrooveTrackingContainer::drawTrackingPointsToMat_HeightOnly(TBIClass_OpenCVMatContainer &_mats)
+{
+
+}
+
+void TBIWeld_VGrooveTrackingContainer::setJointDefinition()
+{
+
+}
+
+
--- tbiweld_vgroovetrackingcontainer.h
+++ tbiweld_vgroovetrackingcontainer.h
@@ -10,6 +10,28 @@
 #include "tbiparameterclass_motioncontrolparams.h"
 #include "tbicore_gary.h"
 #include "tbiclass_opencvmatcontainer.h"
+#include "tbiparameterclass_motioncontrolparams.h"
+
+
+/*
+ class TBIWeld_VGrooveTrackingContainer
+    Is the container class that houses the V Groove Joint Tracking Extensions.
+    It is part of the Processing Pipeline That is responsible of making sure the
+    processed joint points make sense. This is done after the Geometric Construction
+    of the image is handled.
+
+    The Full Auto Joint Set Are Represented By Triangles.
+    Once Successful Tracking Points are Found The System
+    Will Compare it with the definition aquired at the set
+    track to time. The Definition is an overlayed bounded triangle
+    that determines the points define an accurate joint.
+    The system will then create a movement to assert to the
+    track to  points.
+
+
+
+
+ */
 
 class TBIWeld_VGrooveTrackingContainer : public QObject
 {
@@ -17,14 +39,24 @@ class TBIWeld_VGrooveTrackingContainer : public QObject
 public:
     explicit TBIWeld_VGrooveTrackingContainer(QObject *parent = nullptr);
 
+    void setTrackToPoints();
     TBIWeld_ProcessingPipeLineReturnType::PipelineReturnType_t extractTrackingPointsFromGeometricEntities(TBILine &_left_tsl, TBILine &_right_tsl, TBILine &_left_bwl, TBILine &_right_bwl, TBIClass_OpenCVMatContainer &_mats);
-    TBIWeld_ProcessingPipeLineReturnType::PipelineReturnType_t extractTrackingPointsForHeightOnly(TBILine &_tsl, TBIClass_OpenCVMatContainer &_mats);
-
-    void setTrackToPoints(GaryControlMode::ControlMode_t _forcontrolmode = GaryControlMode::TBI_CONTROL_MODE_FULLAUTO_MODE);
-    bool movementNeeded();
+    TBIWeld_ProcessingPipeLineReturnType::PipelineReturnType_t checkTracking();
     int getXMovement(TBIMotionControlParameters &_motioncontrolparams);
     int getZMovement(TBIMotionControlParameters &_motioncontrolparams);
-    void drawTrackToPointstoMat(TBIClass_OpenCVMatContainer &_mats, GaryControlMode::ControlMode_t _forcontrolmode = GaryControlMode::TBI_CONTROL_MODE_FULLAUTO_MODE);
+
+
+
+    void drawTrackToPointstoMat_FullAuto(TBIClass_OpenCVMatContainer &_mats);
+    void drawTrackToPointstoMat_HeightOnly(TBIClass_OpenCVMatContainer &_mats);
+    void drawTrackingPointsToMat_FullAuto(TBIClass_OpenCVMatContainer &_mats);
+    void drawTrackingPointsToMat_HeightOnly(TBIClass_OpenCVMatContainer &_mats);
+    void setJointDefinitionBoundaryOffset(int _offset){m_joint_def_boundary = _offset;}
+    void setMotionRequirementBoundary(int _offset){m_movement_boundary = _offset;}
+
+private:
+        void setJointDefinition();
+
 
 private:
     const bool m_showdebug = false;
@@ -33,20 +65,28 @@ private:
     TBIPoint_Int m_root_point;
     TBIPoint_Int m_joint_centroid;
 
+    //Joint Track To Point
     TBIPoint_Int m_left_track_to_point;
     TBIPoint_Int m_right_track_to_point;
     TBIPoint_Int m_root_track_to_point;
     TBIPoint_Int m_joint_track_to_centroid;
 
+    //Joint Definition. Set At Set Time
     TBIPoint_Int m_left_joint_def;
     TBIPoint_Int m_right_joint_def;
     TBIPoint_Int m_root_joint_def;
     TBIPoint_Int m_joint_centroid_def;
 
-    int m_tracking_boundary;
+    int m_joint_def_boundary;
+    int m_movement_boundary;
+
+    //Height Only Parameters
+    TBILine m_top_surface_line;
+    TBIPoint_Int m_tsl_mid_point;
 
     const cv::Scalar m_tracking_point_cv_color =CV_RGB(0,200,255);
     const cv::Scalar m_tracking_to_point_cv_color =CV_RGB(75,75,255);
+    const cv::Scalar m_boundary_fill_cv_color = CV_RGB(25,200,25);
 
 signals:
 
